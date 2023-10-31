97d4cd050abf1bb602f7a95effad6d1548236563
Moenes Gueni <moenes.gueni@esprit.tn>
2023-10-31 22:13:36 +0100

    first commit

---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AM.ApplicationCore.Domain;
+
+Console.WriteLine("Hello, World!");
+
+Plane plane = new Plane();
+plane.Capacity = 100;
+plane.ManufactureDate = DateTime.Now;
+plane.PlaneType = PlaneType.Boeing;
+
+//Initialiseur d'objets
+Plane plane2 = new Plane { Capacity=200
+                          ,ManufactureDate= new DateTime(2023,09,17)
+                          , PlaneType=PlaneType.Airbus};
+//Afficher plane1
+Console.WriteLine(plane2);
